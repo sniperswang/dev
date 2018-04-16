f192d3f60c2128fcf5de3d4c6d1a3dfa8f8ca708
Yao Wang <sniperswy@gmail.com>
2018-04-16 20:20:05 +0000

    add L294

---
+++ leetcode/L294/test.py
@@ -0,0 +1,28 @@
+"""
+You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, 
+you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer 
+make a move and therefore the other person will be the winner.
+
+Write a function to determine if the starting player can guarantee a win.
+
+For example, given s = "++++", return true. The starting player can guarantee a win by flipping the middle "++" to become "+--+".
+
+Follow up:
+Derive your algorithm's runtime complexity.
+"""
+
+class Solution(object):
+    def canWin(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        _memo = {}
+        def canWin(self,s):
+            memo = self._memo
+            if s not in memo:
+                memo[s] = any(s[i:i+2] == "++" and not self.canWin(s[:i]+'-'+s[i+2:]) for i in range(len(s)))
+            return memo[s]	
+
+
+
